af98a909ac3fbe6e28b2ecd21829e33edc9b8cbe
susji <susji256@gmail.com>
2024-04-08 22:57:28 +0300

    internal/state: Failing test to verify that older updates are not effective

--- internal/state/state_test.go
+++ internal/state/state_test.go
@@ -3,7 +3,9 @@ package state_test
 import (
 	"maps"
 	"testing"
+	"time"
 
+	"github.com/susji/ruuvi/data/rawv2"
 	"github.com/susji/ruuvimetrics/internal/state"
 )
 
@@ -37,6 +39,33 @@ func TestEmptyStateGetting(t *testing.T) {
 		t.Error()
 	}
 	if !maps.Equal(s.SequenceNumbers(), state.Uint16s{}) {
+		t.Error()
+	}
+}
+
+func TestUpdateWithOlder(t *testing.T) {
+	s := state.New()
+	now := time.Now()
+	past := now.Add(-1 * time.Hour)
+	mac := rawv2.MAC{Value: [6]byte([]byte("123456"))}
+	s.Update(&rawv2.RuuviRawV2{
+		Timestamp: now,
+		MAC:       mac,
+		MovementCounter: rawv2.MovementCounter{
+			Valid: true,
+			Value: 100,
+		},
+	})
+	s.Update(&rawv2.RuuviRawV2{
+		Timestamp: past,
+		MAC:       mac,
+		MovementCounter: rawv2.MovementCounter{
+			Valid: true,
+			Value: 20,
+		},
+	})
+	mov := s.MovementCounters()
+	if got, _ := mov[mac]; got.Value != 100 {
 		t.Error()
 	}
 }
